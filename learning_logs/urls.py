b9adeb5794fa7a20154b027ef6a8139c283bfda6
Da1anna <1318176575@qq.com>
2020-07-08 10:35:53 +0800

    应用程序learning_logs完成

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -18,4 +18,8 @@ urlpatterns = [
     url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
     #添加新主题
     url(r'^new_topic/$', views.new_topic, name='new_topic'),
+    #添加新条目
+    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+    #编辑既有条目
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
 ]

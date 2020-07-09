c2fd4ae797411264358602b0f714a3c9f9ec06ce
Da1anna <1318176575@qq.com>
2020-07-09 18:04:19 +0800

    Chapter 20.1 设置网页样式

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -41,9 +41,13 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #第三方应用程序
+    'bootstrap3',
     #我的应用程序
     'learning_logs',
     'users',
+
 )
 
 MIDDLEWARE_CLASSES = (
@@ -110,3 +114,8 @@ STATIC_URL = '/static/'
 
 #我的设置
 LOGIN_URL = '/users/login/'
+
+#django-bootstrap3的设置
+BOOTSTRAP3 = {
+    'include_jquery':True,
+}
